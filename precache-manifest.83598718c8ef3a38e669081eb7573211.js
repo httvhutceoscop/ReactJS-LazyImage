fd791e871d7e9ada92ea64b5c02fb4f9dc82683c
VietNT <viet.nguyen@fujitechjsc.com>
2019-04-19 11:11:19 +0700

    Updates

---
+++ precache-manifest.83598718c8ef3a38e669081eb7573211.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "286acff10c7ec294404788136b4e848b",
+    "url": "/ReactJS-LazyImage/static/media/08.286acff1.jpg"
+  },
+  {
+    "revision": "c13e447179f24b5fab1f",
+    "url": "/ReactJS-LazyImage/static/css/main.aa8220c8.chunk.css"
+  },
+  {
+    "revision": "849a4b5b8bf08d670b5a",
+    "url": "/ReactJS-LazyImage/static/js/runtime~main.6d254198.js"
+  },
+  {
+    "revision": "9d8ea91a2d44c3706f66",
+    "url": "/ReactJS-LazyImage/static/js/2.9efe5921.chunk.js"
+  },
+  {
+    "revision": "c62ad7d5454ceb649de844b7b4f05878",
+    "url": "/ReactJS-LazyImage/static/media/01.c62ad7d5.jpg"
+  },
+  {
+    "revision": "6851c6ee50ce661d15c6dcb6529a8ff4",
+    "url": "/ReactJS-LazyImage/static/media/02.6851c6ee.jpg"
+  },
+  {
+    "revision": "d2e9a990728745aa2981368f5909e8e0",
+    "url": "/ReactJS-LazyImage/static/media/03.d2e9a990.jpg"
+  },
+  {
+    "revision": "4811168ffa53b29e5c26e9de10fd5ad4",
+    "url": "/ReactJS-LazyImage/static/media/04.4811168f.jpg"
+  },
+  {
+    "revision": "7364be44ccfb9ba8e2bc36056b1698dd",
+    "url": "/ReactJS-LazyImage/static/media/05.7364be44.jpg"
+  },
+  {
+    "revision": "b2c9ff85e37ed09b49bc1328ceadca45",
+    "url": "/ReactJS-LazyImage/static/media/06.b2c9ff85.jpg"
+  },
+  {
+    "revision": "a54820e6361e6423b3677a57f62f1513",
+    "url": "/ReactJS-LazyImage/static/media/07.a54820e6.jpg"
+  },
+  {
+    "revision": "c13e447179f24b5fab1f",
+    "url": "/ReactJS-LazyImage/static/js/main.c8ee7f2a.chunk.js"
+  },
+  {
+    "revision": "663ecdfb47e369691d113b441a78683b",
+    "url": "/ReactJS-LazyImage/static/media/09.663ecdfb.jpg"
+  },
+  {
+    "revision": "558a8794da5916e9c6caeeb840394662",
+    "url": "/ReactJS-LazyImage/static/media/10.558a8794.jpg"
+  },
+  {
+    "revision": "df00c4cba11e93706b597911d2b28087",
+    "url": "/ReactJS-LazyImage/static/media/11.df00c4cb.jpg"
+  },
+  {
+    "revision": "dd68c7750b4adb5705a05400c79a13a0",
+    "url": "/ReactJS-LazyImage/static/media/12.dd68c775.jpg"
+  },
+  {
+    "revision": "7c0145a710d2be83480f85a692564318",
+    "url": "/ReactJS-LazyImage/static/media/13.7c0145a7.jpg"
+  },
+  {
+    "revision": "bd0230a4cedddf44fe3112812a7d795e",
+    "url": "/ReactJS-LazyImage/static/media/14.bd0230a4.jpg"
+  },
+  {
+    "revision": "82b405c5c07efa617f5e32f5e1d03187",
+    "url": "/ReactJS-LazyImage/static/media/15.82b405c5.jpg"
+  },
+  {
+    "revision": "8656d7ac5fad8bc05ebcadaf4eb8e39c",
+    "url": "/ReactJS-LazyImage/static/media/16.8656d7ac.jpg"
+  },
+  {
+    "revision": "1a403c78cfe91328675160a99a71740e",
+    "url": "/ReactJS-LazyImage/static/media/17.1a403c78.jpg"
+  },
+  {
+    "revision": "1b768b160587fbbaa50562933c6f541f",
+    "url": "/ReactJS-LazyImage/static/media/18.1b768b16.jpg"
+  },
+  {
+    "revision": "fed77e5867c58f1d1e08f6309d53c289",
+    "url": "/ReactJS-LazyImage/index.html"
+  }
+];
